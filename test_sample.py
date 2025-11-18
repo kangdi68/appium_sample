86b359356f798c1feaa396aafaba9be14ab50424
kangdi68 <kangdi68@gmail.com>
2025-11-18 10:35:11 -0800

    Update test_sample.py

--- test_sample.py
+++ test_sample.py
@@ -1,10 +1,11 @@
 import pytest
+from core import Core
 
 @pytest.mark.usefixtures('init_driver')
 class TestSample(Core):
   settings_tab = (AppiumBy.ACCESSIBILITY_ID, 'Settings')
   new_settings = (AppiumBy.ACCESSIBILITY_ID, 'new settings')
-  
+  core = Core(self.driver)
   def test_sample(self, get_current_test_name):
-      self.click(self..settings_tab)
-      self.wait_until_element_visible(self.new_settings)
+      core.click(self..settings_tab)
+      core.wait_until_element_visible(self.new_settings)
